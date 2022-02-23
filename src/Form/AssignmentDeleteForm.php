1eabac6547962b48849ffa068d6f6494557d7b61
Philippe von Bergen <ph.vonbergen@iqual.ch>
2022-02-23 14:00:48 +0000

    Add missing implementations

--- src/Form/AssignmentDeleteForm.php
+++ src/Form/AssignmentDeleteForm.php
@@ -3,14 +3,27 @@
 namespace Drupal\iq_publisher\Form;
 
 use Drupal\Core\Entity\ContentEntityConfirmFormBase;
+use Drupal\Core\Url;
 
 /**
  * Provides a form for deleting Assignment entities.
  *
  * @ingroup iq_publisher
  */
-class AssignmentDeleteForm extends ContentEntityConfirmFormBase
-{
+class AssignmentDeleteForm extends ContentEntityConfirmFormBase {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function getQuestion() {
+    return $this->t('Are you sure you want to delete %name?', ['%name' => $this->entity->label()]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCancelUrl() {
+    return new Url('entity.assignment.collection');
+  }
 
 }
